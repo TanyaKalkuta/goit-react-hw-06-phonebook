606df0220bb67680baabef3986af9c3872f429e1
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-07-13 18:38:23 +0300

    убрала лишнюю деструктуризацию

--- src/redux/phonebook/phonebook-action.js
+++ src/redux/phonebook/phonebook-action.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createAction } from '@reduxjs/toolkit';
 
-const addContact = createAction('contacts/add', ({ name, number }) => ({
+const addContact = createAction('contacts/add', (name, number) => ({
   payload: {
     id: uuidv4(),
     name,
